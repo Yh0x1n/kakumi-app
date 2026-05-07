22dd1384ea063526a9743b059586216fd644191c
Yh0x1n <yhoxrossell508@gmail.com>
2026-05-07 09:10:27 -0400

    Refining UI for improvement

--- kakumi_app/components/kata_scoreboard.py
+++ kakumi_app/components/kata_scoreboard.py
@@ -6,23 +6,35 @@ Módulo de puntuación de Kata
 import reflex as rx
 
 
+class JudgesState(rx.State):
+    score_fields: list[str] = ["J1", "J2", "J3", "J4", "J5"]
+
+
+def judge_panel() -> rx.Component:
+    return rx.hstack(
+        rx.foreach(
+            JudgesState.score_fields,
+            lambda field: rx.input(
+                placeholder=field,
+                width="95px",
+                height="75px",
+                font_size="45px",
+                text_align="center",
+            ),
+        )
+    )
+
+
 def kata_scoreboard() -> rx.Component:
     return rx.center(
         rx.vstack(
             rx.heading("KATA", size="9"),
             rx.vstack(
-                rx.text("AKA", size="8", as_="label"),
-                width="100%",
-                bg="red",
-                align="start",
-                border_radius="5px",
+                rx.text("ATLETA", size="8", as_="label"),
+                judge_panel(),
+                bg="gray",
+                align="center",
             ),
-            rx.vstack(
-                rx.text("AO", size="8", as_="label"),
-                width="100%",
-                bg="blue",
-                align="end",
-                border_radius="5px",
-            ),
+            align="center",
         )
     )
