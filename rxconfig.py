98defb423ba27bc2cae52c5964c89fd32e7af561
Yh0x1n <yhoxrossell508@gmail.com>
2026-06-15 14:28:46 -0400

    bruh

--- rxconfig.py
+++ rxconfig.py
@@ -2,11 +2,11 @@ import reflex as rx
 
 config = rx.Config(
     app_name="kakumi_app",
-    api_url="http://app.kakumitm.com:8000",
-    backend_path="/api",
+    # api_url="http://app.kakumitm.com:8000",
+    # backend_path="/api",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ],
-    db_url="sqlite:///kakumi.db"
+    db_url="sqlite:///kakumi.db",
 )
